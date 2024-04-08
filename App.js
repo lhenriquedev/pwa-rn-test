a831a21a1ff5a9e24f18b99cd508af54f1497fb3
Henrique <lhenrique.dev@gmail.com>
2024-04-08 14:39:34 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Eai Gerson, abrindo React Native na Web!</Text>
       <StatusBar style="auto" />
     </View>
   );
