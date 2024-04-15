b6a34d7e244b88e423d58dfdbe26df287ff5051c
Gerson Rocha <gersonrocha9@gmail.com>
2024-04-15 09:08:39 -0300

    feat: add onabet link on home button

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
-import { Platform } from 'react-native'
+import { Linking, Platform } from 'react-native'
 
 import { Box } from '../../../components/Box/Box'
 import { Button } from '../../../components/Button/Button'
 import { Screen } from '../../../components/Screen/Screen'
 import { AppTabScreenProps } from '../../../routes'
 
-export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
+export function HomeScreen({ }: AppTabScreenProps<'HomeScreen'>) {
   const isAndroid = Platform.OS === 'android'
   const isIOS = Platform.OS === 'ios'
   const isWeb = Platform.OS === 'web'
 
   return (
     <Screen title="Home" flex={1} bg="backgroundColor">
-      {(isAndroid || isIOS) && <Button title="Link para fora" />}
+      {(isAndroid || isIOS) && (
+        <Button
+          title="Link para fora"
+          onPress={() => {
+            Linking.openURL('https://onabet.com/casino')
+          }}
+        />
+      )}
 
       {isWeb && (
         <Box flex={1}>
-          <iframe
-            frameBorder={0}
-            src="https://onabet.com/casino"
-            height={'100%'}
-          />
+          <iframe src="https://onabet.com/casino" height="100%" />
         </Box>
       )}
     </Screen>
